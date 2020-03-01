f7a1e6c9921cb2969b8c2b999c87c0d7c6e6f7f2
sk zhao <34225286+zhaoshoukuan@users.noreply.github.com>
2020-03-01 21:20:31 +0800

    Add files via upload

--- computewave.py
+++ computewave.py
@@ -13,7 +13,7 @@
 # here put the import lib
 import numpy as np, time
 from qulab.wavepoint import WAVE_FORM as WF
-from qulab.waveform import CosPulseDrag, Expi, DC, Step, Gaussian
+from qulab.waveform import CosPulseDrag, Expi, DC, Step, Gaussian, CosPulse
 from tqdm import tqdm_notebook as tqdm
 
 
@@ -21,33 +21,32 @@ t_list = np.linspace(0,100000,250000)
 t_range = (-90e-6, 10e-6)
 sample_rate = 2.5e9
 
+
 ################################################################################
 ### Rabi波形
 ################################################################################
 
 async def rabiWave(awg,during=75e-9, shift=-200e-9, Delta_lo=80e6,name=['Ex_I','Ex_Q']):
-        lo = Expi(2*np.pi*Delta_lo)
-        #init = CosPulseDrag(during,alpha=0.5,Delta=266e6) << 0.5*during
-        init = (Step(2e-9)<<during) - Step(2e-9)
-        wav = (init >> shift) * lo
+    def genXY(during, phi, shift, Delta_lo=80e6):
+        lo = Expi(2*np.pi*Delta_lo, phi)
+        m1 = CosPulse(during) << during/2
+        m2 = CosPulse(during) << during*3/2
+        m = m1 + m2
+        wav = (m >> shift) * lo
         wav.set_range(*t_range)
         points = wav.generateData(sample_rate)
-        init1 = (Step(0e-9)<<during) - Step(0e-9)
-        wav1 = (init1 >> shift) 
-        wav1.set_range(*t_range)
-        points1 = wav1.generateData(sample_rate)
+        return np.real(points), np.imag(points)
     
-        I, Q = np.real(points), np.imag(points)
+    I, Q = genXY(during,0,shift,Delta_lo)
+    await awg.update_waveform(I, name[0])
+    await awg.update_waveform(Q, name[1])
+    #await awg.update_marker(name[1],mk1=points1)
 
-        await awg.update_waveform(I, name[0])
-        await awg.update_waveform(Q, name[1])
-        await awg.update_marker(name[1],mk1=points1)
-
-async def updateWaveform_rabi(measure,kind,awg,t_rabi=None):
+async def Rabi_sequence(measure,kind,awg,t_rabi=None):
     if t_rabi == None:
         t_rabi = np.linspace(0,100,501)
     await measure.awg[awg].stop()
     time.sleep(5)
     for j,i in tqdm(enumerate(t_rabi,start=0),desc='Rabi_sequence'):
         name_ch = [measure.wave[kind][0][j],measure.wave[kind][1][j]]
-        await updateWaveform_rabi(measure.awg[awg],during=i/1e9, name = name_ch)
+        await rabiWave(measure.awg[awg],during=i/1e9, name = name_ch)
