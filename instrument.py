52aa1ff1ed3558aa5933d45d37bdcefb84879d91
sk zhao <34225286+zhaoshoukuan@users.noreply.github.com>
2020-03-20 23:11:49 +0800

    Add files via upload

--- instrument.py
+++ instrument.py
@@ -31,7 +31,7 @@ class qubitCollections():
         self.qubits = {i.q_name:i for i in qubits}
         qstate = {}
         for i in self.qubits:
-            qstate[i] = {'dc':0,'ex':0,'read':False}
+            qstate[i] = {'dc':0,'bias_z':0,'ex':0,'read':False}
         self.__setattr__('qstate',qstate)
         if calimatrix != None:
             self.calimatrix = calimatrix
@@ -42,48 +42,82 @@ class qubitCollections():
         n = len(f_cavity)
         return f_lo, delta, n
 
-    def exMixing(self,f_ex):
+    def exMixing(self,f):
+        qname = [i[0] for i in f]
+        f_ex = np.array([i[1] for i in f])
         ex_lo = f_ex.mean() + 50e6
-        delta_ex =  ex_lo - f_ex
+        delta =  ex_lo - f_ex
+        delta_ex = {qname[i]:delta[i] for i in range(len(qname))}
         # n = len(f_ex)
         return ex_lo, delta_ex
 
     def qubitExecute(self,state=None):
-        if state != None:
-            for i in state:
-                self.qstate[i] = state[i]
-        else:
-            pass
+#         if state != None:
+#             for i in state:
+#                 self.qstate[i] = state[i]
+#         else:
+#             pass
         #bias
-        if hasattr(self,'calimatrix'):
-            bias = []
-            for i,j in enumerate(self.qubits):
+        qall = {'dc':{},'awg131':{},'awg132':{},'awg133':{},'awg134':{},'awg_read':{}}
+        bias, bias_z = [], []
+        for i in self.qubits:
+            if i in state and 'dc' in state[i]:
                 bias.append(state[i]['dc'])
+            else:
+                bias.append(self.qstate[i]['dc'])
+            if i in state and 'bias_z' in state[i]:
+                bias_z.append(state[i]['bias_z'])
+            else:
+                bias_z.append(self.qstate[i]['bias_z'])
+        if hasattr(self,'calimatrix'):
             bias_cross = np.mat(self.calimatrix).I * np.mat(bias).T
             self.bias_cross = bias_cross
+            bias_cross_z = np.mat(self.calimatrix).I * np.mat(bias_z).T
+            self.bias_cross_z = bias_cross_z
         else:
-            bias = []
-            for i,j in enumerate(self.qubits):
-                bias.append(state[i]['dc'])
             self.bias_cross = np.array(bias)
+            self.bias_cross_z = np.array(bias_z)
         #read
         fread = []
         for i in state:
-            if state[i]['read']:
+            if 'read' in state[i] and state[i]['read']:
                 fread.append(self.qubits[i].f_lo[0])
         f_lo, delta, n = self.readMixing(np.array(fread))
         self.f_lo, self.delta, self.n = f_lo, delta, n
         #excite
         fex1, fex2 = [], []
         for i in self.qubits:
-            if self.qubits[i].inst['ex_lo'] == 'psg_ex1':
-                if state[i]['ex'] == 1:
-                    fex1.append(self.qubits[i].f_ex[0])
-            if self.qubits[i].inst['ex_lo'] == 'psg_ex2':
-                if state[i]['ex'] == 1:
-                    fex2.append(self.qubits[i].f_ex[0])
-        ex_lo1, delta_ex1 = self.exMixing(np.array(fex1))
-        ex_lo2, delta_ex2 = self.exMixing(np.array(fex2))
+            if i in state and 'ex' in state[i]:
+                if self.qubits[i].inst['ex_lo'] == 'psg_ex1':
+                    if state[i]['ex'] != 0:
+                        fex1.append((i,self.qubits[i].f_ex[0]))
+                if self.qubits[i].inst['ex_lo'] == 'psg_ex2':
+                    if state[i]['ex'] != 0:
+                        fex2.append((i,self.qubits[i].f_ex[0]))        
+
+        qall['psg_lo'] = {'Frequncy':self.f_lo}
+        qall['awg_read']['ch'+str(7)] = {'Frequency':self.delta}
+        qall['awg_read']['ch'+str(8)] = {'Frequency':self.delta}
+
+        for i, j in enumerate(self.qubits):
+            qall['dc'][self.qubits[j].inst['dc']] = self.bias_cross[i]
+            qall[self.qubits[j].inst['z_awg']]['ch'+str(self.qubits[j].inst['z_ch'])] = {'Volt':self.bias_cross_z[i]}
+            if fex1 != []:
+                ex_lo1, delta_ex1 = self.exMixing(fex1)
+                qall['psg_ex1'] = {'Frequency':ex_lo1}
+                if j in delta_ex1:
+                    delta_ex = delta_ex1
+                    for k in self.qubits[j].inst['ex_ch']:
+                        qall[self.qubits[j].inst['ex_awg']]['ch'+str(k)] = {'Frequency':delta_ex[j]}
+            if fex2 != []:
+                ex_lo2, delta_ex2 = fex2[0][1], {fex2[0][0]:0}
+                qall['psg_ex2'] = {'Frequency':ex_lo2}
+                if j in delta_ex2:
+                    delta_ex = delta_ex2
+                    for k in self.qubits[j].inst['ex_ch']:
+                        qall[self.qubits[j].inst['ex_awg']]['ch'+str(k)] = {'Frequency':delta_ex[j]}
+        self.qall = qall
+
             
 
 
